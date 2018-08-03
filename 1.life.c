1ce4ff18d60cbf62e00e49b387b6b5d8bea77506
thisistia99 <32358554+thisistia99@users.noreply.github.com>
2018-08-03 10:52:02 +0530

    Create 1.life.c

---
+++ 1.life.c
@@ -0,0 +1,19 @@
+#1.Life, the Universe, and Everything 
+#Problem code-TEST
+
+#include<stdio.h>
+int main()
+{
+	int n;
+	while(1){
+		scanf("%d", &n);
+		if(n!=42)
+			printf("%d ", n);
+		if(n==42)
+			break;
+				
+	}
+		
+return 0;	
+}
+ 
